0df522b4607ad5df4c6295e2108c1c114646aa4b
DavidWei95036593 <dw36593@pausd.us>
2018-01-12 13:50:53 -0800

    UPload the notes

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const argv = require("yargs").argv;
 
-console.log(process.argv[3]);
-//argv means it's a single dimensional array
+const command = argv._[0]
+  if(command ==="add"){
+    console.log("Add function chosen");
+}
+if(command ==="list"){
+  console.log("List function chosen");
+}
+if(command ==="read"){
+  console.log("Read function chosen");
+}
+if(command ==="remove"){
+  console.log("Remove function chosen");
+}
+//add, list, read, remove
