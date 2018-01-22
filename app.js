f6962323a4464570ac71932014f4d04fb5f7f227
DavidWei95036593 <dw36593@pausd.us>
2018-01-22 12:58:02 -0800

    finish the problem

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const argv = require("yargs").argv;
-
+const notes = require("./note.js");
 const command = argv._[0]
 
   if(command ==="add"){
-    console.log("Add function chosen");
+    const title = argv.title;
+    const body = argv.body;
+    // notes.addNote(title,body);
+    const noteAdded = notes.addNote(title,body);
+      if(noteAdded){
+      console.log("Your note was added");
+    }
+    else{
+      console.log("This note already exists");
+    }
   }
   if(command ==="list"){
-    console.log("List function chosen");
+    const title = argv.title;
+    const body = argv.body;
+    notes.listNote(title,body);
   }
   if(command ==="read"){
-    console.log("Read function chosen");
+    const title = argv.title;
+    const body = argv.body;
+    const noderead = notes.readNote(title);
+    if(noderead){
+
+    }
+    else{
+      console.log("no such node exists");
+    }
   }
   if(command ==="remove"){
-    console.log("Remove function chosen");
+    const title = argv.title;
+    const body = argv.body;
+    const nodeRemoved = notes.removeNote(title);
+    if(nodeRemoved){
+    console.log("Your node is removed");
+    }
+    else{
+    console.log("no such node exists");}
+
   }
--- note.js
+++ note.js
@@ -1,27 +1,91 @@
 const fs = require('fs');
-const fetchNotes=()=>{
-  const noteString = fs.readFileSync("notes-data.json");
-  return JSON.parse(noteString);
+const fetchNotes = () => {
+  try {
+    const notesString = fs.readFileSync("notes-data.json");
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
 }
 
-const addNote =(title, body)=> {
-  const node={
+const saveNotes = (notes) => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+}
+//this is a helper method that check to see if the note exists
+const checkNote = (notes,note)=>{
+  if(notes.every(n=> n.title !=note.title)){
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//this is the add note function that adds the node
+const addNote = (title, body) => {
+  const note = {
     title,
     body
-    /*title:title,
-    body:body*/
   }
   const notes = fetchNotes();
+  if (checkNote(notes,note)){
+    notes.push(note);
+    saveNotes(notes);
+    return true;
+  } else {
+    return false;
+  }
+}
+//this is the removenote function that remove the note
+const removeNote = (title)=>{
+  const note = {
+    title
+  }
+
+  const notes = fetchNotes();
+
+  if (checkNote(notes,note)==false){
+    notes.splice(note.title);
+    saveNotes(notes);
+    return note;
+
+  }
+    else{
+
+      return false;
+    }
 }
 
-const listNote=(title,body)=>{
-
+//this function will read the note out if there is a note
+const readNote = (title)=>{
+    const notes = fetchNotes();
+  const note={
+    title,
+  }
+  if (checkNote(notes,note)==false){
+    console.log(note);
+      return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
-const removeNote=(title,body)=>{
-
+const listNote = () => {
+  const notes = fetchNotes();
+  console.log("Title                    Body");
+  for (i in notes) {
+    let space = " ".repeat(25-notes[i].title.length);
+    console.log(notes[i].title + space + notes[i].body);
+  }
 }
 
-const readNote=(title,body)=>{
 
+
+module.exports = {
+  addNote,
+  removeNote,
+  readNote,
+  listNote
 }
