be4ae99c37039ce37bac9dced598b4b8e3b5ed76
DavidWei95036593 <dw36593@pausd.us>
2018-01-22 13:30:07 -0800

    hi

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const command = argv._[0]
   if(command ==="add"){
     const title = argv.title;
     const body = argv.body;
+
     // notes.addNote(title,body);
     const noteAdded = notes.addNote(title,body);
-      if(noteAdded){
+    // console.log(noteAdded);
+    if(noteAdded){
       console.log("Your note was added");
     }
     else{
+      // console.log(noteAdded);
       console.log("This note already exists");
     }
   }
@@ -24,7 +27,7 @@ const command = argv._[0]
     const body = argv.body;
     const noderead = notes.readNote(title);
     if(noderead){
-
+      console.log();
     }
     else{
       console.log("no such node exists");
@@ -33,6 +36,7 @@ const command = argv._[0]
   if(command ==="remove"){
     const title = argv.title;
     const body = argv.body;
+    // notes.removeNote(title);
     const nodeRemoved = notes.removeNote(title);
     if(nodeRemoved){
     console.log("Your node is removed");
